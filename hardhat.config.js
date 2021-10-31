7e874cff862ba54300c3f6d38a08acb0e4f3b6aa
Aliya Vasilyeva <aliya@divnotes.com>
2021-10-31 16:08:18 +0300

    Test creating and executing market sales

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 // task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 //   const accounts = await hre.ethers.getSigners();
