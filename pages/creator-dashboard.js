73335e06848439c999bad0f2945f84e0f8bb636b
Aliya Vasilyeva <aliya@divnotes.com>
2021-10-31 18:16:41 +0300

    Create dashboard page

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -8,8 +8,9 @@ import { nftaddress, nftmarketaddress } from '../config';
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
-export default function MyAssets() {
+export default function CreatorDashboard() {
   const [nfts, setNfts] = useState([]);
+  const [sold, setSold] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -28,7 +29,7 @@ export default function MyAssets() {
       signer
     );
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
-    const data = await marketContract.fetchMyNFTs();
+    const data = await marketContract.fetchItemsCreated();
 
     const items = await Promise.all(
       data.map(async (i) => {
@@ -48,12 +49,12 @@ export default function MyAssets() {
         return item;
       })
     );
+
+    const soldItems = items.filter((i) => i.sold);
+    setSold(soldItems);
     setNfts(items);
     setLoaded(true);
   }
-
-  if (loaded && !nfts.length)
-    return <h1 className='px-20 py-10 text-3xl'>No assets owned</h1>;
 
   return (
     <div className='flex justify-center'>
@@ -74,6 +75,32 @@ export default function MyAssets() {
             </div>
           ))}
         </div>
+      </div>
+      <div className='px-4'>
+        {Boolean(sold.length) && (
+          <div>
+            <h2 className='text-2xl py-2'>Items sold</h2>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+              {sold.map((nft, i) => (
+                <div
+                  key={i}
+                  className='border shadow rounded-xl overflow-hidden'
+                >
+                  <img
+                    src={nft.image}
+                    alt=' '
+                    style={{ minHeight: 100, margin: 'auto' }}
+                  />
+                  <div className='p-4 bg-black'>
+                    <p className='text-2xl mb-4 font-bold text-white'>
+                      Price - {nft.price} Matic
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
